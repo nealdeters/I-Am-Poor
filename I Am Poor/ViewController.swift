bf25ea90c18acaeab997f5018f27da9b6a968be3
Neal Deters <nealdeters@gmail.com>
2019-11-28 10:40:39 -0600

    created i am poor project

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Neal Deters on 11/28/19.
+//  Copyright © 2019 Neal Deters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
